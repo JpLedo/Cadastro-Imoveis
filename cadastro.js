37b273a737e6b569c6c2b9f37592508dd78a9166
Ledo <joaopedroledoribeiro@gmail.com>
2022-09-01 14:45:36 -0300

    EstudosJS

---
+++ cadastro.js
@@ -0,0 +1,54 @@
+const imoveis = [];
+let opcao = "";
+
+do {
+    opcao = prompt(
+        "Bem-vindo ao Cadastro de Imóveis!  " +
+        "\nTotal de imóveis: " + imoveis.length +
+        "\n\nEscolha uma opção:\n1 - Novo imóvel\n2 - Listar imóveis\n3 - Sair"
+    )
+    switch(opcao){
+        case "1":
+            const imovel = {}
+            imovel.proprietario = prompt("Informe o nome do proprietário")
+            imovel.quartos = prompt("Quantos quartos possui o imóvel? ")
+            imovel.banheiros = prompt("Quantos banheiros possui o imóvel")
+            imovel.garagem = prompt("O imovel possui garagem ? (Sim/Nao)")
+            const confirmacao = confirm(
+                "Salvar este imóvel?\n" +
+                "\nProprietário: " + imovel.proprietario +
+                "\nQuartos: " + imovel.quartos +
+                "\nBanheiros: " + imovel.banheiros +
+                "\nPossui garagem? " + imovel.garagem 
+                
+            )
+            if(confirmacao){
+                imoveis.push(imovel)
+                alert("Imóvel salvo com sucesso")
+            }else{
+                alert("Voltando ao menu")
+            }
+            break;
+        case "2":
+            for(let i = 0; i < imoveis.length ; i++){
+                alert(
+                    "Imóvel " + (i + 1) +
+                    "\nProprietário: " + imoveis[i].proprietario +
+                    "\nQuartos: " + imoveis[i].quartos +
+                    "\nBanheiros: " + imoveis[i].banheiros + 
+                    "\nPossui garagem? " + imoveis[i].garagem 
+                )
+            }
+            break;
+        case "3":
+            alert("Encerrando..")
+            break;
+        default:
+            alert("Opção invalida")
+
+    }
+} while (opcao !== "3") {
+
+}
+
+
